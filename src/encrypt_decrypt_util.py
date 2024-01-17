ce57f1b1d37bed99e7585c25df554f7a1d2d706a
Rafael <rafael.silva@aimopark.se>
2024-01-17 11:05:04 +0100

    Make code pep8 compliant

--- src/encrypt_decrypt_util.py
+++ src/encrypt_decrypt_util.py
@@ -4,11 +4,12 @@ from cryptography.fernet import Fernet
 key = Fernet.generate_key()
 cipher_suite = Fernet(key)
 
+
 def encrypt_string(input_string):
     encrypted_bytes = cipher_suite.encrypt(input_string.encode())
     return encrypted_bytes
 
+
 def decrypt_string(encrypted_bytes):
     decrypted_bytes = cipher_suite.decrypt(encrypted_bytes)
     return decrypted_bytes.decode()
-
